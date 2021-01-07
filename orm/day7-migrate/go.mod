bc82fc48a716cd1d2a934685250ad54ff6bda83f
chengcheng1021 <chengcheng1021@aliyun.com>
2021-01-07 15:16:46 +0800

    数据库迁移

---
+++ orm/day7-migrate/go.mod
@@ -0,0 +1,7 @@
+module geeorm
+
+go 1.15
+
+require github.com/mattn/go-sqlite3 v1.14.5
+
+require github.com/prometheus/common v0.10.0
---
+++ orm/day7-migrate/session/raw.go
@@ -0,0 +1,111 @@
+package session
+
+import (
+	"database/sql"
+	"geeorm/clause"
+	"geeorm/dialect"
+	"geeorm/schema"
+	"github.com/prometheus/common/log"
+	"strings"
+)
+
+type Session struct {
+	//db      *sql.DB
+	//sql     strings.Builder
+	//sqlVars []interface{}
+
+	//db       *sql.DB
+	//dialect  dialect.Dialect
+	//refTable *schema.Schema
+	//sql      strings.Builder
+	//sqlVars  []interface{}
+
+	//db       *sql.DB
+	//dialect  dialect.Dialect
+	//refTable *schema.Schema
+	//clause   clause.Clause
+	//sql      strings.Builder
+	//sqlVars  []interface{}
+
+	db       *sql.DB
+	dialect  dialect.Dialect
+	tx       *sql.Tx
+	refTable *schema.Schema
+	clause   clause.Clause
+	sql      strings.Builder
+	sqlVars  []interface{}
+}
+
+func New(db *sql.DB, dialect dialect.Dialect) *Session {
+	//return &Session{db: db}
+
+	return &Session{
+		db:      db,
+		dialect: dialect,
+	}
+}
+
+func (s *Session) Clear() {
+	//s.sql.Reset()
+	//s.sqlVars = nil
+
+	s.sql.Reset()
+	s.sqlVars = nil
+	s.clause = clause.Clause{}
+}
+
+func (s *Session) DB() *sql.DB {
+	return s.db
+}
+
+func (s *Session) Raw(sql string, values ...interface{}) *Session {
+	s.sql.WriteString(sql)
+	s.sql.WriteString(" ")
+	s.sqlVars = append(s.sqlVars, values...)
+	return s
+}
+
+// Exec raw sql with sqlVars
+func (s *Session) Exec() (result sql.Result, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if result, err = s.DB().Exec(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err)
+	}
+	return
+}
+
+// QueryRow gets a record from db
+func (s *Session) QueryRow() *sql.Row {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	return s.DB().QueryRow(s.sql.String(), s.sqlVars...)
+}
+
+// QueryRows gets a list of records from db
+func (s *Session) QueryRows() (rows *sql.Rows, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if rows, err = s.DB().Query(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err)
+	}
+	return
+}
+
+// CommonDB is a minimal function set of db
+type CommonDB interface {
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+	QueryRow(query string, args ...interface{}) *sql.Row
+	Exec(query string, args ...interface{}) (sql.Result, error)
+}
+
+var _ CommonDB = (*sql.DB)(nil)
+var _ CommonDB = (*sql.Tx)(nil)
+
+// DB returns tx if a tx begins. otherwise return *sql.DB
+func (s *Session) DB() CommonDB {
+	if s.tx != nil {
+		return s.tx
+	}
+	return s.db
+}
